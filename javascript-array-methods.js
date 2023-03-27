0fd96d184cc74729cbcff401b074c209327a7b28
ratroot92 <ratroot92@gmail.com>
2023-03-27 17:01:36 +0500

    first commit

---
+++ javascript-array-methods.js
@@ -0,0 +1,78 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// 1. Array Fill Method
+
+// Fill Syntax:
+// arr.fill(value, start, end)
+// value => to fill the array with
+// start => start index, default it 0
+// end => end index is arr.length, always executed
+
+// var players = ["Babar", "Smith", "Kohli"];
+
+// new_players = players.fill("Butler", 0, 2)
+
+// console.log("PLAYERS ... ", new_players);
+
+// 2. Array Constructor Method
+
+// 3. Array Copy Within Method
+// let words = ["apple", "ball", "cat", "dog"];
+// words.copyWithin(1,3)
+// console.log(words);
+
+// 4. Array Entries Method
+// const alphabets = ["A", "B", "C"];
+
+// let iterator = alphabets.entries();
+
+// for (let entry of iterator) {
+//     console.log(entry);
+// }
+
+// 5. Array Every Method
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// let result = numbers.every(element => element < 6);
+
+// console.log("RESULT ... ", result);
+
+// 6. Array Filter Method
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function checkEven(n) {
+//     if (n % 2 == 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// let evenNumbers = numbers.filter(checkEven)
+
+// console.log("EVEN NUMBERS >>> ", evenNumbers)
+
+// const languages = ["JavaScript", "Python", "Ruby", "C", "C++", "Swift", "PHP", "Java"];
+
+// function searchFor (arr, query) {
+//     function condition (element) {
+//         return element.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+//     }
+//     return arr.filter(condition)
+// }
+
+// let newArr = searchFor(languages, "c");
+
+// console.log(newArr);
+
+
+
+
+
+
+
+
+
